237e53033499d1e4e77be6793c43834fd909e930
viccarre <viccarre@gmail.com>
2015-05-14 13:36:19 -0400

    Water Level updating with timer

--- Pump Controll/ViewController.swift
+++ Pump Controll/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var temp: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let baseURL = NSURL(string: "https://agent.electricimp.com/qDvrHlrYvYDp?ultrasonic")
+        //Convert Data to String
+        let data = NSData(contentsOfURL: baseURL!)
+        let datastring = NSString(data: data!, encoding:NSUTF8StringEncoding)
+        println(datastring!)
+        temp.text = datastring! as String
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func update() {
+        // Do any additional setup after loading the view, typically from a nib.
+        let baseURL = NSURL(string: "https://agent.electricimp.com/qDvrHlrYvYDp?ultrasonic")
+        //Convert Data to String
+        let data = NSData(contentsOfURL: baseURL!)
+        let datastring = NSString(data: data!, encoding:NSUTF8StringEncoding)
+        println(datastring!)
+        temp.text = datastring! as String
     }
 
     override func didReceiveMemoryWarning() {
